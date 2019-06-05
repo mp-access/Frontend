b2245f05a4f7a721523dd08e198df1f960eda4bf
soyabeen <sein.anschluss@gmail.com>
2019-06-05 17:33:56 +0200

    Merge simple exercise view with code page #60.

--- src/pages/Exercise.js
+++ src/pages/Exercise.js
@@ -1,45 +1,105 @@
 import React, {Component} from 'react';
 import {withAuth} from "../auth/AuthProvider";
 import CourseDataService from "../utils/CourseDataService";
+import MonacoEditor from "react-monaco-editor";
+import utils from "../utils";
 
 const ReactMarkdown = require('react-markdown');
 
 class Exercise extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            excercise: undefined
-        }
-    }
+
+    state = {
+        language: 'none',
+        code: 'Choose code file above',
+        isLoading: false,
+        files: [],
+        question: 'Is Loading ... ',
+    };
 
     componentDidMount() {
-        const exerciseId = this.props.match.params.exerciseId;
-        (async () => {
-            CourseDataService.getExercise(exerciseId)
+        this.setState({ isLoading: true });
 
-                .then(res => res.json())
-                .then(
-                    result => this.setState({excercise: result})
-                )
-                .catch(err => {
-                    console.debug("Error:", err.toString())
-                });
-        })();
+        this.fetchMyExercise()
+            .then(() => this.setState({ isLoading: false }));
     }
 
-    render() {
-        if (!this.state.excercise) {
-            return null;
+    //refactor query to not be hardcoded to exercise but to receive input argument
+    fetchMyExercise = async () => {
+        const exerciseId = this.props.match.params.exerciseId;
+
+        const { context } = this.props;
+        const response = await CourseDataService.getExercise(exerciseId);
+
+        if (response.ok) {
+            const content = await response.json();
+
+            const files = content.public_files;
+
+            this.setState({ files, question: content.question });
         }
+    };
+
+    //monaco editor
+    editorDidMount = (editor, monaco) => {
+        console.log('editorDidMount', editor);
+        editor.focus();
+        console.log(monaco);
+    };
+
+    onChange = (newValue, e) => {
+        console.log('onChange', newValue, e);
+    };
+
+    //setting code editor language within tabs
+    setPython = () => this.setState({ language: 'python' });
+    setJs = () => this.setState({ language: 'javascript' });
+
+    setCode = (code) => this.setState({ code: code });
+
+    setTab = (extension, code) => {
+        if (extension === 'py') {
+            this.setPython();
+        } else if (extension === 'js') {
+            this.setJs();
+        }
+        this.setCode(code);
+    };
+
+    render() {
+
+        const options = {
+            selectOnLineNumbers: true,
+        };
+
+        const tabItems = this.state.files.map((c) =>
+            <button key={c.name}
+                    onClick={() => this.setTab(c.extension, c.content)}>{c.name + '.' + c.extension}</button>,
+        );
 
         return (
-            <div>
-                <h2>{this.state.excercise.id}</h2>
 
-                <div>
-                    <ReactMarkdown source={this.state.excercise.question}/>
-                </div>
+            <div className="CodeEditor" style={{ width: '100%' }}>
+
+                <ReactMarkdown source={this.state.question}/>
+
+                {/* render files as tabs */}
+                {tabItems}
+
+                <MonacoEditor
+                    width="100%"
+                    height="1000px"
+                    language={this.state.language}
+                    theme="vs-dark"
+                    value={this.state.code}
+                    automaticLayout={true}
+                    options={options}
+                    quickSuggestions={true}
+                    snippetSuggestions={true}
+                    wordBasedSuggestions={true}
+                    onChange={this.onChange}
+                    editorDidMount={this.editorDidMount}
+                />
             </div>
         );
     }
