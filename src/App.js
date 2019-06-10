5d508da8632d014e36f1e70a72ff06aa523b9a10
Alexander Hofmann <alexhofmann@gmail.com>
2019-06-10 12:10:55 +0200

    Fix bootstrap import link for prod

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import {AuthProvider} from './auth/AuthProvider';
 import AppNavigation from './Navigation/AppNavigation';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends Component {
