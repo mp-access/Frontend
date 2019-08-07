8b35dc49391d5a391bc4c66e3a538fafcfda2594
Philip Hofmann <philip.hofmann@uzh.ch>
2019-08-07 11:27:00 +0200

    Options form for multiple choice

--- src/components/choice/ChoiceExercise.js
+++ src/components/choice/ChoiceExercise.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactMarkdown from 'react-markdown';
 
 class ChoiceExercise extends Component {
@@ -11,19 +11,38 @@ class ChoiceExercise extends Component {
     }
 
     componentDidMount = async () => {
-        const { exercise } = this.props;
+        const {exercise} = this.props;
 
         this.setState({
             question: exercise.question,
+            options: exercise.options
         });
     };
 
+
     render() {
+        var options = [];
+        var optionsLength = 0;
+        if(this.state.options){ optionsLength = this.state.options.length;}
+        for (var i = 0; i < optionsLength; i++) {
+            // note: we add a key prop here to allow react to uniquely identify each
+            // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+            options.push(<input type="checkbox" name="multipleChoiceOption" value={this.state.options[i]} key={i}/>);
+            options.push(" " + this.state.options[i]);
+            options.push(<br/>);
+        }
         return (
             <>
                 <div className="row">
                     <div className="col-12">
                         <ReactMarkdown source={this.state.question}/>
+                    </div>
+                    <div className="col-12">
+                        <form>
+                            {options}
+                            <input type="submit" value="Submit"/>
+                        </form>
+
                     </div>
                 </div>
             </>
