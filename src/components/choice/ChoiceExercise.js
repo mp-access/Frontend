5e38b9640822385597245aaa3aa1b4ba390b6ef5
Philip Hofmann <philip.hofmann@uzh.ch>
2019-08-14 14:48:44 +0200

    ChoiceExercise store answers in state

--- src/components/choice/ChoiceExercise.js
+++ src/components/choice/ChoiceExercise.js
@@ -7,18 +7,52 @@ class ChoiceExercise extends Component {
         super(props);
         this.state = {
             question: undefined,
+            singleChoiceValue: '',
+            multipleChoiceValue: []
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount = async () => {
         const {exercise} = this.props;
-        console.log(exercise);
 
         this.setState({
             question: exercise.question,
             options: exercise.options,
             type: exercise.type
         });
+    };
+
+    handleChange(event) { //add options to array or remove them if unclicked
+        if (this.state.type === 'singleChoice') {
+            this.setState({singleChoiceValue: event.target.value});
+        } else if (this.state.type === 'multipleChoice') {
+            let multipleChoiceArray = this.state.multipleChoiceValue;
+            if (this.state.multipleChoiceValue.includes(event.target.value)) {
+                multipleChoiceArray = multipleChoiceArray.filter(e => e !== event.target.value);
+            } else {
+                multipleChoiceArray.push(event.target.value);
+            }
+            this.setState({multipleChoiceValue: multipleChoiceArray});
+        }
+    }
+
+    handleSubmit(event) {
+        if (this.state.type === 'singleChoice') {
+            alert('This option has been submitted: ' + this.state.singleChoiceValue);
+        } else if (this.state.type === 'multipleChoice') {
+            alert('This array of options has been submitted: ' + this.state.multipleChoiceValue);
+        }
+        event.preventDefault();
+    }
+
+    getPublicFiles = () => {
+        if (this.state.type === 'singleChoice') {
+            return this.state.singleChoiceValue;
+        } else if (this.state.type === 'multipleChoice') {
+            return this.state.multipleChoiceValue;
+        }
     };
 
     render() {
@@ -40,7 +74,7 @@ class ChoiceExercise extends Component {
             // note: we add a key prop here to allow react to uniquely identify each
             // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
             options.push(<input type={type} name={name} value={this.state.options[i]}
-                                key={i}/>);
+                                key={i} onChange={this.handleChange}/>);
             options.push(" " + this.state.options[i]);
             options.push(<br/>);
         }
@@ -52,7 +86,7 @@ class ChoiceExercise extends Component {
                         <ReactMarkdown source={this.state.question}/>
                     </div>
                     <div className="col-12">
-                        <form>
+                        <form onSubmit={this.handleSubmit}>
                             {options}
                             <br/>
                             <input type="submit" value="Submit"/>
