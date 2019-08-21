8b8c035a4674e66a603bcc6d0368de213774df8e
Philip Hofmann <philip.hofmann@uzh.ch>
2019-08-21 14:37:52 +0200

    + .catch statements for fetch in CourseDataService

--- src/utils/CourseDataService.js
+++ src/utils/CourseDataService.js
@@ -4,12 +4,12 @@ class CourseDataService {
 
     static async getCourses(authHeader) {
         return fetch(utils.courseServiceUrl + '/courses', authHeader())
-            .then(result => result.json());
+            .then(result => result.json()).catch(error => console.error('Error: ', error));
     }
 
     static async getAssignment(courseId, assignmentId, authHeader) {
         return fetch(utils.courseServiceUrl + '/courses/' + courseId + '/assignments/' + assignmentId, authHeader())
-            .then(result => result.json());
+            .then(result => result.json()).catch(error => console.error('Error: ', error));
     }
 
     static async getExercise(exerciseId, authHeader) {
@@ -18,12 +18,12 @@ class CourseDataService {
                 if (response.ok) {
                     return response.json();
                 }
-            });
+            }).catch(error => console.error('Error: ', error));
     }
 
     static async getExerciseFile(exerciseId, fileId, authHeader) {
         return fetch(utils.courseServiceUrl + '/exercises/' + exerciseId + "/files/" + fileId, authHeader())
-            .then(result => result.blob());
+            .then(result => result.blob()).catch(error => console.error('Error: ', error));
     }
 
 }
