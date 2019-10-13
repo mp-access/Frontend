0d5fb945c4c1163a636a865c0646d9669cf69b24
Haeri <Haeri@users.noreply.github.com>
2019-10-14 01:48:15 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,14 @@ const Footer = () => (
         <div className={"container-fluid"}>
             <div className="row">
                 <div className="col-6">
-                    <div>© 2019 ACCESS</div>
-                    <small>with ❤️ by <a target="_blank" rel="noopener noreferrer" href="https://github.com/mp-access">ACCESS-team</a></small>
+                    <small>© 2019 ACCESS</small>
+                    <br/>
+                    <small>With ❤️ by <a target="_blank" rel="noopener noreferrer" href="https://github.com/mp-access">ACCESS-Team</a></small>
                 </div>
                 <div className="col-6">
                     <div className="float-right">
-                        <div><small>Frontend: <strong>v{version}</strong></small></div>
-                        <div><small>Backend: <strong>v{Util.serverInfo().version}</strong></small></div>
+                        <div><small>Frontend: <strong>{version}</strong></small></div>
+                        <div><small>Backend: <strong>{Util.serverInfo().version}</strong></small></div>
                     </div>
                 </div>
             </div>
