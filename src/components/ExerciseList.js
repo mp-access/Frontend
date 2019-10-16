23cba749515c1c7e53d7aafbff6b0e8fc2efb5d8
Philip Hofmann <philip.hofmann@uzh.ch>
2019-10-16 20:11:10 +0200

    show task number as reference for students

--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import Util from '../utils/Util';
-import { Code, Type, CheckCircle, CheckSquare, AlertCircle } from 'react-feather';
-import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import {Code, Type, CheckCircle, CheckSquare, AlertCircle} from 'react-feather';
+import {OverlayTrigger, Tooltip} from 'react-bootstrap';
 
 class ExerciseList extends Component {
 
 
-    getIcon(type){
+    getIcon(type) {
         let icon;
         switch (type) {
             case "code":
@@ -30,16 +30,9 @@ class ExerciseList extends Component {
     }
 
     render() {
-        const { exercises, selectedId, gradedSubmissions, showScore } = this.props;
+        const {exercises, selectedId, gradedSubmissions, showScore} = this.props;
 
         const listItems = exercises.map((e, index) => {
-
-            let longTitle;
-            if (e.longTile){
-                longTitle = <small>{e.longTile}</small>;
-              } else {
-                longTitle = <small>{this.getIcon(e.type)} {Util.humanize(e.type)} {(e.type === 'code' || e.type === 'codeSnippet') ? '(' + Util.humanize(e.language) + ')' : ''}</small>;
-            }
 
             const gradedSub = gradedSubmissions ? gradedSubmissions.find(gs => gs.exerciseId === e.id) : undefined;
 
@@ -47,34 +40,36 @@ class ExerciseList extends Component {
             const maxScore = e.maxScore ? e.maxScore : 1;
 
             return (
-                <li key={index} className={"h-flex" + (selectedId === e.id ? ' active' : '') + (gradedSub ? '' : ' fresh')}>
+                <li key={index}
+                    className={"h-flex" + (selectedId === e.id ? ' active' : '') + (gradedSub ? '' : ' fresh')}>
                     <Link to={`/exercises/${e.id}`} className="flex-grow-1">
-                        <span>{e.title ? e.title : 'Task ' + (index+1)}</span>
+                        <span>{e.title ? e.title : 'Task ' + (index + 1)}</span>
                         <p>{!e.isGraded ? ' (Bonus)' : ''}</p>
-                        {longTitle}
+                        {/*  <small>{this.getIcon(e.type)} {Util.humanize(e.type)} {(e.type === 'code' || e.type === 'codeSnippet') ? '(' + Util.humanize(e.language) + ')' : ''}</small>  */}
+                        <small>{'Task ' + (index + 1)}</small>
                         {showScore ? '' : <div align="right"><small>{score} / {maxScore}</small></div>}
                     </Link>
 
                     {(gradedSub && gradedSub.invalid) &&
-                        <div>
-                            <OverlayTrigger
-                                placement="top"
-                                overlay={
-                                    <Tooltip id="tooltip-outdated">
-                                        This submission is outdated!
-                                    </Tooltip>
-                                }
-                            >
-                                <span className="style-btn warn"><AlertCircle size={14} /></span>
-                            </OverlayTrigger>
-                            <span className="p-1"></span>
-                        </div>
+                    <div>
+                        <OverlayTrigger
+                            placement="top"
+                            overlay={
+                                <Tooltip id="tooltip-outdated">
+                                    This submission is outdated!
+                                </Tooltip>
+                            }
+                        >
+                            <span className="style-btn warn"><AlertCircle size={14}/></span>
+                        </OverlayTrigger>
+                        <span className="p-1"></span>
+                    </div>
                     }
-                    
+
                     {showScore &&
-                        <div className="score-display">
-                            <span className="style-btn ghost">Score: {score} / {maxScore}</span>
-                        </div>
+                    <div className="score-display">
+                        <span className="style-btn ghost">Score: {score} / {maxScore}</span>
+                    </div>
                     }
                 </li>
             );
